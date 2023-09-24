05a686df10cc12714e04472a87631f451ddfa6dd
raja <kushwah8458@gmail.com>
2023-09-24 22:46:59 +0530

    Smart Vocabulary App

--- app/src/main/java/com/example/smartvocab/MainActivity.java
+++ app/src/main/java/com/example/smartvocab/MainActivity.java
@@ -45,7 +45,9 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
 
-                if (id==R.id.optHistory) {
+                if (id==R.id.optHome) {
+                    loadFragment(new Content_main());
+                }else if (id==R.id.optHistory) {
                     loadFragment(new AFragment());
                 } else if (id==R.id.optStarred) {
                     loadFragment(new BFragment());
